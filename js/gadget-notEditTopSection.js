7616fc9fffc78e6b094ed321b6bbfacc216bf568
bhsd <2545473905@qq.com>
2021-03-03 07:33:46 -0500

    Create gadget-notEditTopSection.js

---
+++ js/gadget-notEditTopSection.js
@@ -0,0 +1,7 @@
+/**
+ * @Function: 手机版顶部编辑按钮编辑全文而非序言
+ * @Dependencies: mediawiki.util
+ * @Author: https://llwiki.org/zh/User:Bhsd
+ */
+"use strict";
+$('#ca-edit').attr('href', mw.util.getUrl(null, {action: 'submit'})).removeAttr( 'title' );
