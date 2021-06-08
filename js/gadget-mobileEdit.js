0cac3476cf993092bc295c46cf70afa6e393e1ee
bhsd <2545473905@qq.com>
2021-06-08 10:00:18 -0400

    update gadget-mobileEdit

--- js/gadget-mobileEdit.js
+++ js/gadget-mobileEdit.js
@@ -7,17 +7,19 @@
 const id = mw.config.get( 'wgRevisionId' ),
     cid = mw.config.get( 'wgCurRevisionId' ),
     isText = mw.config.get( 'wgPageContentModel' ) == 'wikitext';
-// 注意不能用常用的#bodyContent；这里涵盖了创建新页面、从移动版差异编辑和模板中常见的编辑链接
-$('#content').on('click', 'a.new, .mw-mf-diff-info__link-latest > a, .plainlinks > a.external', function() {
-    $(this).attr('href', function(i, attr) { return attr.replace('&action=edit', '&action=submit'); });
+// 必须在drawer生成之前执行；这里涵盖了创建新页面、从移动版差异编辑和模板中常见的编辑链接
+mw.hook( 'wikipage.content' ).add(function($content) {
+    console.log('Hook: wikipage.content, 开始更换编辑链接');
+    $content.find('a.new, .mw-mf-diff-info__link-latest > a, .plainlinks > a.external')
+        .attr('href', function(i, attr) { return attr.replaceAll('&action=edit', '&action=submit'); });
 });
 $(function() { // Ajax小工具一般不会生成新的.edit-page，所以只需执行一次
     if (mw.config.get( 'wgIsArticle' ) === 0) { return; }
     // 不能用startsWith，因为data-section可能未定义
-    $( 'a.edit-page' ).filter(function() { return /^T-/.test( this.dataset.section ); }).remove();
+    $('a.edit-page').filter(function() { return /^T-/.test( this.dataset.section ); }).remove();
     const notTop = mw.isModule( 'notEditTopSection', true );
     // 这里希望在MobileFrontEnd的JS加载之前执行，但如果晚了也没问题
-    $( 'a.edit-page' ).off( 'click' ).on('click', function(e) {
+    $('a.edit-page').off( 'click' ).click(function(e) {
         e.stopImmediatePropagation();
         const section = this.dataset.section || (isText && !notTop ? 0 : undefined);
         location.href = mw.util.getUrl(null, $.extend( {}, // 移除无效的query parameter
--- otherwiki/gadget-mobileEdit.js
+++ otherwiki/gadget-mobileEdit.js
@@ -4,24 +4,20 @@
  * @Author: https://llwiki.org/zh/User:Bhsd
  */
 "use strict";
-const id = mw.config.get( 'wgRevisionId' ),
-    cid = mw.config.get( 'wgCurRevisionId' ),
-    isText = mw.config.get( 'wgPageContentModel' ) == 'wikitext';
-// 注意不能用常用的#bodyContent；这里涵盖了创建新页面、从移动版差异编辑和模板中常见的编辑链接
-$('#content').on('click', 'a.new, .mw-mf-diff-info__link-latest > a, .plainlinks > a.external', function() {
-    $(this).attr('href', function(i, attr) { return attr.replace('&action=edit', '&action=submit'); });
-});
-$(function() { // Ajax小工具一般不会生成新的.edit-page，所以只需执行一次
+$(() => { // Ajax小工具一般不会生成新的.edit-page，所以只需执行一次
+    const id = mw.config.get( 'wgRevisionId' ),
+        cid = mw.config.get( 'wgCurRevisionId' );
+    if (['edit', 'submit'].includes( mw.config.get( 'wgAction' ) )) {
+        mw.loader.addStyleTag( '.mw-editform span.cancelLink { display: inline-block; }' );
+    }
     if (mw.config.get( 'wgIsArticle' ) === 0) { return; }
     // 不能用startsWith，因为data-section可能未定义
-    $( 'a.edit-page' ).filter(function() { return /^T-/.test( this.dataset.section ); }).remove();
-    const notTop = mw.isModule( 'notEditTopSection', true );
+    $('a.edit-page').filter(function() { return /^T-/.test( this.dataset.section ); }).remove();
     // 这里希望在MobileFrontEnd的JS加载之前执行，但如果晚了也没问题
-    $( 'a.edit-page' ).off( 'click' ).on('click', function(e) {
+    $('a.edit-page').off( 'click' ).click(e => {
         e.stopImmediatePropagation();
-        const section = this.dataset.section || (isText && !notTop ? 0 : undefined);
         location.href = mw.util.getUrl(null, $.extend( {}, // 移除无效的query parameter
-            {action: 'submit', oldid: id == cid ? undefined : id, section: section}
+            {action: 'submit', oldid: id == cid ? undefined : id, section: this.dataset.section}
         ));
     }).removeAttr( 'href' );
 });
