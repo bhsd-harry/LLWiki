04e2024fa500a71aa3a99424f836050295df46dd
bhsd <2545473905@qq.com>
2021-06-14 17:18:24 -0400

    Update gadget-mobileEdit.js

--- otherwiki/gadget-mobileEdit.js
+++ otherwiki/gadget-mobileEdit.js
@@ -1,25 +1,29 @@
 /**
  * @Function: 手机版打开普通编辑界面
- * @Dependencies: mediawiki.util, ext.gadget.site-lib
+ * @Dependencies: mediawiki.util
  * @Author: https://llwiki.org/zh/User:Bhsd
  */
 "use strict";
-const f = () => { // Ajax小工具一般不会生成新的.edit-page，所以只需执行一次
+$(() => { // Ajax小工具一般不会生成新的.edit-page，所以只需执行一次
     const id = mw.config.get( 'wgRevisionId' ),
-        cid = mw.config.get( 'wgCurRevisionId' );
+        oldid = mw.config.get( 'wgCurRevisionId' ) == id ? undefined : id;
     if (['edit', 'submit'].includes( mw.config.get( 'wgAction' ) )) {
         mw.loader.addStyleTag( '.mw-editform span.cancelLink { display: inline-block; }' );
     }
     if (mw.config.get( 'wgIsArticle' ) === 0) { return; }
+    $('.talk').off( 'click' ).click(e => { e.stopImmediatePropagation(); });
     // 不能用startsWith，因为data-section可能未定义
-    $('a.edit-page').filter(function() { return /^T-/.test( this.dataset.section ); }).remove();
+    $('.edit-page').filter(function() { return /^T-/.test( this.dataset.section ); }).remove();
     // 这里希望在MobileFrontEnd的JS加载之前执行，但如果晚了也没问题
-    $('a.edit-page').off( 'click' ).click(function(e) {
+    $('.edit-page').off( 'click' ).click(function(e) {
         e.stopImmediatePropagation();
-        location.href = mw.util.getUrl(null, $.extend( {}, // 移除无效的query parameter
-            {action: 'submit', oldid: id == cid ? undefined : id, section: this.dataset.section}
-        ));
-    }).removeAttr( 'href' );
+        location.href = this.href;
+    });
+    $('#mw-mf-viewport')[0].addEventListener('click', e => {
+        const $target = $(e.target);
+        if ($target.parent().attr( 'id' ) != 'ca-edit') { return; }
+        e.stopPropagation();
+        $target.removeAttr( 'href' );
+        location.href = mw.util.getUrl(null, $.extend({action: 'edit'}, {oldid}));
+    }, true);
 };
-if (document.readyState == 'complete') { f(); }
-else { $(window).on('load', f); }
