951500bacfe8671503fcca7fa276674c3811873a
bhsd <2545473905@qq.com>
2021-06-14 08:01:04 -0400

    Update gadget-mobileEdit.js

--- otherwiki/gadget-mobileEdit.js
+++ otherwiki/gadget-mobileEdit.js
@@ -4,7 +4,7 @@
  * @Author: https://llwiki.org/zh/User:Bhsd
  */
 "use strict";
-$(() => { // Ajax小工具一般不会生成新的.edit-page，所以只需执行一次
+$(window).on('load', () => { // Ajax小工具一般不会生成新的.edit-page，所以只需执行一次
     const id = mw.config.get( 'wgRevisionId' ),
         cid = mw.config.get( 'wgCurRevisionId' );
     if (['edit', 'submit'].includes( mw.config.get( 'wgAction' ) )) {
@@ -14,7 +14,7 @@ $(() => { // Ajax小工具一般不会生成新的.edit-page，所以只需执�
     // 不能用startsWith，因为data-section可能未定义
     $('a.edit-page').filter(function() { return /^T-/.test( this.dataset.section ); }).remove();
     // 这里希望在MobileFrontEnd的JS加载之前执行，但如果晚了也没问题
-    $('a.edit-page, #ca-edit > a').off( 'click' ).click(function(e) {
+    $('a.edit-page').off( 'click' ).click(function(e) {
         e.stopImmediatePropagation();
         location.href = mw.util.getUrl(null, $.extend( {}, // 移除无效的query parameter
             {action: 'submit', oldid: id == cid ? undefined : id, section: this.dataset.section}
