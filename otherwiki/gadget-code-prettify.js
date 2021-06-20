92cbd2d91654ffea5ce8008547cee083467f8f9c
bhsd <2545473905@qq.com>
2021-06-19 22:39:44 -0400

    problematic return

--- otherwiki/gadget-code-prettify.js
+++ otherwiki/gadget-code-prettify.js
@@ -32,55 +32,6 @@
             $content.find( '.lang-lua' ).addClass( 'lua' );
             $content.find( '.lang-html' ).addClass( 'xml' );
         }
-        const $block = $content.find( '.hljs:not(.highlighted)' );
-        if ($block.length) { // 高亮非Wikitext代码
-            console.log('Hook: wikipage.content, 开始执行语法高亮');
-            try {
-                await (window.hljs ? Promise.resolve() : Promise.all([
-                    $.ajax(highlight_path[0], {dataType: 'script', cache: true}),
-                    mw.loader.load(highlight_path[1], 'text/css')
-                ]));
-                // 1. 语法高亮
-                $block.each(function() { hljs.highlightBlock( this ); }).addClass( 'highlighted' ).filter( '.linenums' )
-                    .html(function() { // 添加行号。这里不使用<table>排版，而是使用<ol>
-                    const $this = $(this),
-                        start = $this.data( 'start' ) || $this.data( 'line-from' ) || 1;
-                    $this.children( ':contains(\n)' ).replaceWith(function() { // 先处理跨行元素
-                        const $self = $(this);
-                        return $self.html().split( '\n' )
-                            .map(ele => $self.clone().html( ele ).prop( 'outerHTML' )).join('\n');
-                    });
-                    const lines = $this.html().replace(/\n$/, '').split( '\n' );
-                    return $('<ol>', { start,
-                        html: lines.map((ele, i) => $('<li>', {html: ele, id: `L${i + start}`}))
-                    }).css('padding-left', `${(lines.length + start - 1).toString().length + 2.5}ch`);
-                });
-                // 2. 手动跳转
-                const fragment = decodeURIComponent( location.hash.slice(1) ),
-                    target = document.getElementById( fragment || null ); // 用户输入内容，禁止使用$()
-                if (/^L\d+$/.test( fragment ) && target) { target.scrollIntoView({ behavior: 'smooth' }); }
-                // 3. 对CSS代码添加指示色块
-                const $cssblock = $block.filter( '.css' );
-                if ($cssblock.length === 0) { return; }
-                const $color = $('<span>', {class: 'hljs-color'});
-                $cssblock.find( '.hljs-number:contains(#)' ).before(function() { // 16进制颜色
-                    const color = this.textContent,
-                        n = color.length,
-                        alpha = n == 5 ? color[4].repeat(2) : color.slice(7);
-                    return $color.clone().css({ color: color.slice(0, n > 5 ? 7 : 4),
-                        opacity: alpha ? parseInt(alpha, 16) / 255 : undefined });
-                });
-                $cssblock.find( '.hljs-built_in:contains(rgb), .hljs-built_in:contains(hsl)' ).before(function() { // RGB颜色
-                    const $siblings = $(this).parent().contents(), // 标点符号都是text节点，所以需要使用contents
-                        index = $siblings.index( this ),
-                        n = this.textContent.length == 4 ? 9 : 7, // rgba/hsla或rgb/hsl
-                        // 右半括号那一项可能有分号
-                        color = [...$siblings.slice(index, index + n)].map(({textContent: text}) => text);
-                    return $color.clone().css({ color: color[0].slice(0, 3) + color.slice(1, 7).join('') + ')', // eslint-disable-line
-                        opacity: color[8] });
-                });
-            } catch { mw.notify('无法下载highlight.js，代码高亮失败！', {type: 'error'}); }
-        }
 
         const $code = $content.find( '.lang-wiki' );
         let mwConfig;
@@ -104,6 +55,55 @@
                 });
             } catch { mw.notify('无法下载CodeMirror扩展，Wikitext高亮失败！', {type: 'error'}); }
         }
+
+        const $block = $content.find( '.hljs:not(.highlighted)' );
+        if ($block.length === 0) { return; } // 高亮非Wikitext代码
+        console.log('Hook: wikipage.content, 开始执行语法高亮');
+        try {
+            await (window.hljs ? Promise.resolve() : Promise.all([
+                $.ajax(highlight_path[0], {dataType: 'script', cache: true}),
+                mw.loader.load(highlight_path[1], 'text/css')
+            ]));
+            // 1. 语法高亮
+            $block.each(function() { hljs.highlightBlock( this ); }).addClass( 'highlighted' ).filter( '.linenums' )
+                .html(function() { // 添加行号。这里不使用<table>排版，而是使用<ol>
+                const $this = $(this),
+                    start = $this.data( 'start' ) || $this.data( 'line-from' ) || 1;
+                $this.children( ':contains(\n)' ).replaceWith(function() { // 先处理跨行元素
+                    const $self = $(this);
+                    return $self.html().split( '\n' )
+                        .map(ele => $self.clone().html( ele ).prop( 'outerHTML' )).join('\n');
+                });
+                const lines = $this.html().replace(/\n$/, '').split( '\n' );
+                return $('<ol>', { start,
+                    html: lines.map((ele, i) => $('<li>', {html: ele, id: `L${i + start}`}))
+                }).css('padding-left', `${(lines.length + start - 1).toString().length + 2.5}ch`);
+            });
+            // 2. 手动跳转
+            const fragment = decodeURIComponent( location.hash.slice(1) ),
+                target = document.getElementById( fragment || null ); // 用户输入内容，禁止使用$()
+            if (/^L\d+$/.test( fragment ) && target) { target.scrollIntoView({ behavior: 'smooth' }); }
+            // 3. 对CSS代码添加指示色块
+            const $cssblock = $block.filter( '.css' );
+            if ($cssblock.length === 0) { return; }
+            const $color = $('<span>', {class: 'hljs-color'});
+            $cssblock.find( '.hljs-number:contains(#)' ).before(function() { // 16进制颜色
+                const color = this.textContent,
+                    n = color.length,
+                    alpha = n == 5 ? color[4].repeat(2) : color.slice(7);
+                return $color.clone().css({ color: color.slice(0, n > 5 ? 7 : 4),
+                    opacity: alpha ? parseInt(alpha, 16) / 255 : undefined });
+            });
+            $cssblock.find( '.hljs-built_in:contains(rgb), .hljs-built_in:contains(hsl)' ).before(function() { // RGB颜色
+                const $siblings = $(this).parent().contents(), // 标点符号都是text节点，所以需要使用contents
+                    index = $siblings.index( this ),
+                    n = this.textContent.length == 4 ? 9 : 7, // rgba/hsla或rgb/hsl
+                    // 右半括号那一项可能有分号
+                    color = [...$siblings.slice(index, index + n)].map(({textContent: text}) => text);
+                return $color.clone().css({ color: color[0].slice(0, 3) + color.slice(1, 7).join('') + ')', // eslint-disable-line
+                    opacity: color[8] });
+            });
+        } catch { mw.notify('无法下载highlight.js，代码高亮失败！', {type: 'error'}); }
     };
     mw.hook( 'wikipage.content' ).add($content => { main($content); });
 })();
